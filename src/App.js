4191f6809294282fd553f90e02ce205a0688a612
Dawid <dawidjura12@gmail.com>
2023-01-26 14:21:12 +0100

    Created and styled Navbar component

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ const App = () => {
       </div>
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats Bussines Billing CardDeal Testimonials Clients Cta Footer
+          {/* Stats Bussines Billing CardDeal Testimonials Clients Cta Footer */}
         </div>
       </div>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants/constants_data";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
-    <div>
-    Navbar
-    </div>
-  )
-}
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank logo" className="w-[124px] h-[32px]" />
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((navLink, index) => {
+          const { id, title } = navLink;
+          return (
+            <li
+              key={id}
+              className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+                index === navLinks.length - 1 ? "mr-0" : "mr-10"
+              }`}
+            >
+              <a href={`#${id}`}>{title}</a>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          onClick={() => setToggle((prevState) => !prevState)}
+          src={toggle ? close : menu}
+          alt="Hamburger menu icon"
+          className="w-[28px] h-[28px] object-contain cursor-pointer"
+        />
 
-export default Navbar
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((navLink, index) => {
+              const { id, title } = navLink;
+              return (
+                <li
+                  key={id}
+                  className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+                    index === navLinks.length - 1 ? "mb-0" : "mb-4"
+                  }`}
+                >
+                  <a href={`#${id}`}>{title}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
