d900541aca7b4107802c854580cd1863c7fc7512
Dawid <dawidjura12@gmail.com>
2023-01-31 11:36:56 +0100

    Created CardDeal component

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ const App = () => {
           <Stats />
           <Business />
           <Billing />
+          <CardDeal />
           {/* Stats Bussines Billing CardDeal Testimonials Clients Cta Footer */}
         </div>
       </div>
